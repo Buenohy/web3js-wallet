241ef4a749d0a996fea21639babf100528781fe1
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2025-10-19 16:11:36 -0300

    feat: handle Connect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
+'use client';
+import { getProvider } from '@/contexts/Web3Provider';
+import { useState } from 'react';
+
 export default function Home() {
+  const [address, setAddress] = useState(null);
+  const [message, setMessage] = useState('');
+
+  const handleConnect = async () => {
+    try {
+      const { account } = await getProvider();
+      setAddress(account);
+      setMessage(`Carteira conectada: ${account}`);
+    } catch (err) {
+      setMessage(err.message);
+    }
+  };
+
   return (
     <div className="p-4">
       <h1 className="text-center">Web3 Js Wallet</h1>
       <div className="flex flex-col gap-4">
-        <h3>Wallet</h3>
+        <div>
+          <h3>Wallet</h3>
+          <input
+            type="text"
+            value={address}
+            onChange={(evt) => setAddress(evt.target.value)}
+          />
+        </div>
         <h3>From</h3>
         <h3>To</h3>
         <h3>Block</h3>
--- src/contexts/Web3Provider.tsx
+++ src/contexts/Web3Provider.tsx
@@ -1,3 +1,13 @@
 import Web3 from 'web3';
 
-const web3 = new Web3(Web3.givenProvider);
+export async function getProvider() {
+  if (!window.ethereum) throw new Error(`No Wallet found!`);
+
+  const web3 = new Web3(window.ethereum);
+
+  const accounts = await web3.eth.requestAccounts();
+  console.log(accounts);
+  if (!accounts || !accounts.length) throw new Error(`Permission required!`);
+
+  return { web3, account: accounts[0] };
+}
